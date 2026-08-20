fc586f3e26f8ffaa022fdd35acaf157c1ff4e377
Tolux490 <antolux@wp.pl>
2026-08-20 23:31:16 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   // Zmień na własną domenę przed wdrożeniem — potrzebne do sitemap i tagów OG
-  site: 'https://TWOJ-LOGIN.github.io',
+  site: 'https://Tolux490.github.io',
   // Odkomentuj TYLKO przy GitHub Pages w repo projektowym:
   base: '/portfolio-stron',
 });
